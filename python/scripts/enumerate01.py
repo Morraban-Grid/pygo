e02ce6a5fb6a04ab9b507dcabf189e4a26dec47c
Morraban-Grid <andromeda.4i.1000@gmail.com>
2026-03-28 19:46:14 -0500

    feat: add enumerate sentence.

---
+++ python/scripts/enumerate01.py
@@ -0,0 +1,14 @@
+"""
+Example of enumerate in Python.
+"""
+
+print("Example of enumerate in Python.\n\n")
+
+# Create a list of items
+sueldos = [1000, 2000, 3000, 4000, 5000, 10000, 20000, 30000, 40000, 50000]
+# Creamos un diccionario con el índice y el sueldo utilizando enumerate
+index_sueldos = {index : sueldo for index, sueldo in enumerate(sueldos, start=1)}
+
+print("Index and sueldos:")
+for index, sueldo in index_sueldos.items():
+    print(f"Index: {index}, Sueldo: {sueldo}")
