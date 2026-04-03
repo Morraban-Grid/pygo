bfe0e0f73069375399d447d25a1d6a2d66efac2c
Morraban-Grid <andromeda.4i.1000@gmail.com>
2026-04-02 20:12:15 -0500

    feat: collection's features.

--- python/scripts/ejemplo.py
+++ python/scripts/ejemplo.py
@@ -1,6 +1,6 @@
-num1 = 5
-print(id(num1))
-num1 = 6
-print(id(num1))
-num2 = 6
-print(id(num2))
+print("this is the matrix")
+
+matrix = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+print(matrix)
+matrix = tuple(matrix)
+print(matrix)
