75c65c3128696542f1b9f59add3924676ad5e341
Morraban-Grid <andromeda.4i.1000@gmail.com>
2026-04-02 19:58:24 -0500

    feat: int feature.

---
+++ python/scripts/dict01.py
@@ -0,0 +1,17 @@
+
+lista01 = []
+dict01 = {}
+tuple01 = ()
+set01 = set()
+numero01 = 0
+string01 = ""
+decimal01 = 0.0
+
+lista_01 = [lista01, dict01, tuple01, set01, numero01, string01, decimal01]
+
+
+#for i in lista_01:
+#    print(bool(i))
+
+#print(list(map(bool, lista_01)))
+print([i for i in lista_01])
---
+++ python/scripts/ejemplo.py
@@ -0,0 +1,6 @@
+num1 = 5
+print(id(num1))
+num1 = 6
+print(id(num1))
+num2 = 6
+print(id(num2))
