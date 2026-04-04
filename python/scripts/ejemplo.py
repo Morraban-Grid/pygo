6a34770415818fa16d74812e0e84c8e7ee3f9535
Morraban-Grid <andromeda.4i.1000@gmail.com>
2026-04-03 20:43:51 -0500

    feat: add operators.

--- python/scripts/ejemplo.py
+++ python/scripts/ejemplo.py
@@ -1,6 +1,21 @@
-print("this is the matrix")
+print("operadores aritmeticos")
 
-matrix = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
-print(matrix)
-matrix = tuple(matrix)
-print(matrix)
+a = 10
+b = 5
+print("suma: ", a + b)
+print("resta: ", a - b)
+print("multiplicacion: ", a * b)
+print("division: ", a / b)
+print("operadores de asignacion")
+c = 10
+c += 5
+print("c += 5: ", c)    
+
+lista01 = [1, 2, 3]
+lista02 = [4, 5, 6]
+print("operadores de pertenencia")
+print("3 in lista01: ", 3 in lista01)
+print("4 in lista01: ", 4 in lista01)
+print("operadores de identidad")
+print("lista01 is lista02: ", lista01 is lista02)
+print("lista01 is lista01: ", lista01 is lista01)
