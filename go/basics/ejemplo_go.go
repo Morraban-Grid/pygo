e92bd91494894137c9d2fc6d3decd2be527b8c00
Morraban-Grid <andromeda.4i.1000@gmail.com>
2026-04-06 18:00:49 -0500

    feat: add new go routine.

--- go/basics/ejemplo_go.go
+++ go/basics/ejemplo_go.go
@@ -26,4 +26,34 @@ func main(){
 	filas := len(A) //len(A) es el número de filas de A
 	columnas := len(A[0]) //len(A[0]) es el numero de columnas de A
 
+	//Creamos la matriz resultado C con las mismas dimensiones que A y B
+	//Esta matriz es de tipo int
+	C := make([][]int, filas)
+
+	/*
+	Vamos a iterar sobre cada fila de C
+	y para cada i vamos a crear un slice de tipo int
+	con la misma cantidad de columnas de A y B.
+	*/
+	for i := range C{
+		C[i] = make([]int, columnas)
+	}
+
+	//Usamos WaitGroup para esperar a que todas las goroutines terminen
+	var wg sync.WaitGroup
+
+	//Iteramos sobre cada elemento de la matriz C
+	for i := 0; i < filas; i++{
+		//Avisamos que hay una nueva tarea
+		wg.Add(1)
+
+		//Lanzamos una goroutine para cada calcular cada fila
+		go func(i int){
+			//Al terminar la función, avisamos que la tarea ha terminado
+			defer wg.Done()
+			for j := 0; j < columnas; j++{
+				c[i][j] = A[i][j] + B[i][j]
+		}
+	}(i)
+
 }
